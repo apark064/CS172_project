d6d8f096313c8f285e3afdb7c0d29d46d549afed
Masashi Yamaguchi <59752780+myama048@users.noreply.github.com>
2021-05-29 16:15:44 -0700

    almost done with part 1

--- crawler.py
+++ crawler.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup  # install BeautifulSoup4
-import requests  # install requests
+import requests  # install requests and lxml
 
 def crawler(num_page, num_level):
-    seed_url = requests.get('http://ucr.edu').text
+    source = requests.get('http://ucr.edu').text
+    seed_url = 'http://ucr.edu'
     queue = []
     iter_page = 1  # queue.len()
     iter_level = 1 # depth from seed_url
@@ -10,8 +11,9 @@ def crawler(num_page, num_level):
     while queue and iter_level < num_level:
         iter_level += 1
         # get all url and put them in the queue (beautiful soup)
-        soup = BeautifulSoup(seed_url, 'lxml')
+        soup = BeautifulSoup(source, 'lxml')
         for url in soup.find_all('a', href=True):  # need to fix
+            #print(url['href'])
             if iter_page < num_page:
                 iter_page += 1
                 if url not in queue:  # URL Duplicate Reduction
