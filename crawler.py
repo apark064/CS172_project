4ba9aae982e21fd49ce4de312e30a35ff8a5f292
Masashi Yamaguchi <59752780+myama048@users.noreply.github.com>
2021-05-29 17:10:54 -0700

    added robot class

--- crawler.py
+++ crawler.py
@@ -1,25 +1,47 @@
 from bs4 import BeautifulSoup  # install BeautifulSoup4
 import requests  # install requests and lxml
+import pandas as pd
 
-def crawler(num_page, num_level):
-    source = requests.get('http://ucr.edu').text
-    seed_url = 'http://ucr.edu'
+def robot(url): # returns disallowed contents
+    robots_url = url + '/robots.txt'
+    print(robots_url)
+    robots = requests.get(robots_url).text
+    data = []
+    lines = str(robots).splitlines()
+    #print(lines)
+    for line in lines:
+
+        if line.strip():
+            if not line.startswith('#'):
+                split = line.split(':', maxsplit=1)
+                if(split[0].strip() == 'Disallow'):
+                    data.append([split[1].strip()])
+    #print(pd.DataFrame(data, columns=['parameter']))
+    return pd.DataFrame(data, columns=['parameter'])
+
+def crawler(seed, num_page, num_level):
+    seed_url = seed
+    source = requests.get(seed_url).text
     queue = []
     iter_page = 1  # queue.len()
     iter_level = 1 # depth from seed_url
     queue.append(seed_url)
+    disallowed = robot(seed)
     while queue and iter_level < num_level:
         iter_level += 1
         # get all url and put them in the queue (beautiful soup)
         soup = BeautifulSoup(source, 'lxml')
         for url in soup.find_all('a', href=True):  # need to fix
-            #print(url['href'])
-            if iter_page < num_page:
-                iter_page += 1
-                if url not in queue:  # URL Duplicate Reduction
-                    queue.append(url.get('href'))
+            if url not in disallowed: # avoid disallowed contents from robots.txt
+                if iter_page < num_page:
+                    if url not in queue:  # URL Duplicate Reduction
+                        if 'http' in url.get('href'):
+                            queue.append(url.get('href'))
+                            iter_page += 1
         # elastic search
 
     print(queue)
 
-crawler(20, 2)
+seed_url = 'https://www.ucr.edu'
+crawler(seed_url, 20, 2)
+#robot(seed_url)
