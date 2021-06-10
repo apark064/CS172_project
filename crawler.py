07cc5e1821bbcc2c0f20310504a842c05a0b60ad
Masashi Yamaguchi <59752780+myama048@users.noreply.github.com>
2021-06-09 21:07:03 -0700

    dramatic progress!

--- crawler.py
+++ crawler.py
@@ -7,11 +7,14 @@ import json
 from datetime import datetime
 from time import sleep
 from elasticsearch import Elasticsearch, helpers
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def robot(url):  # returns disallowed contents
     robots_url = url + '/robots.txt'
-    #print(robots_url)
+    # print(robots_url)
     robots = requests.get(robots_url).text
     data = []
     lines = str(robots).splitlines()
@@ -49,6 +52,7 @@ def crawler(seed, num_page, num_level):
     return queue
     # print(queue)
 
+
 def get_body_text(link):
     html = urllib.request.urlopen(link)
     page_soup = BeautifulSoup(html, "html.parser")
@@ -61,12 +65,40 @@ def get_body_text(link):
     for para in containers:
         str += para.get_text()
         str += '\n'
-        #print(para.get_text())
+        # print(para.get_text())
     return str
+
+
+def sort_list_by_time(list):  # make priority queue by last-updated-time. If not, append back in same order traversed
+    # <head> .. <meta property="og:updated_time" content="2021-06-02T16:43:53-0700">
+    p_queue = []
+    list_without_update = []
+    for item in list:
+        html = urllib.request.urlopen(item)
+        page_soup = BeautifulSoup(html, "html.parser")
+        exist = page_soup.head.find("meta", property="og:updated_time")
+        if exist:
+            p_queue.append([exist["content"], item]) #time, url
+            print(p_queue)
+        else:
+            list_without_update.append(item)
+
+    p_queue = sorted(p_queue, key=lambda x: x[0], reverse=True)
+    return_list = []
+    for thing in range(len(p_queue)):
+        return_list.append(p_queue[thing][1])
+
+    return return_list + list_without_update
+
+link = 'https://www.ucr.edu/'
+list = crawler(link, 10, 2)
+print(list)
+print(sort_list_by_time(list))
+print('end')
 
 link = 'https://www.ucr.edu/'
 link1 = 'https://news.ucr.edu/articles/2021/06/01/2021-voices-grads-share-pivotal-moments-their-educational-journeys'
-#print(get_body_text(link1))
+# print(get_body_text(link1))
 
 
 # ========================================
@@ -83,14 +115,13 @@ elastic_endpoint = "i-o-optimized-deployment-753d85.es.us-west1.gcp.cloud.es.io:
 
 connection_string = "https://elastic:" + elastic_pass + "@" + elastic_endpoint
 
-indexName = "cs172-index"
+indexName = "myindex_1"  # "cs172-index"
 esConn = Elasticsearch(connection_string)
 response = esConn.indices.create(index=indexName, ignore=400)  # create index
 print(response)  # status
 
 
 for link in list:
-    # with open("docs.json", "w") as outfile:
     url = link
     # title = "blah"
     text = get_body_text(link)
@@ -105,4 +136,27 @@ for link in list:
     # json_object = json.dumps(doc, indent=4)  #convert library to json obj
     response = esConn.index(index=indexName, id=id, body=doc)  # add doc to index
     print(response)  # result status
+
+response = esConn.search(index=indexName, body={"query": {"match_all": {}}})
+print(response)  # result status
+
+# think this is how we look up a query?
+
+
+query = "blah"
+response = esConn.search(index=indexName, body={
+        'query': {
+            'match': {
+                "text": query
+            }
+        }
+    })
+
+print(response)  # result status
+
+
+
+#curl -X PUT -u elastic:HdEPP9nkrjsbs0fy7sb7Dztm "https://i-o-optimized-deployment-753d85.es.us-west1.gcp.cloud.es.io:9243/myindex?pretty"    <-- cretate "myindex"
+#curl -X DELETE -u elastic:HdEPP9nkrjsbs0fy7sb7Dztm "https://i-o-optimized-deployment-753d85.es.us-west1.gcp.cloud.es.io:9243/myindex?pretty"    <-- delete "myindex"
+
 
